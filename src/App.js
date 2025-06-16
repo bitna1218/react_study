679cbfd3bd0e542c9d22909669258921ff7b9bc4
bitna <hbn1218@naver.com>
2025-06-17 00:35:52 +0900

    project upload

--- src/App.js
+++ src/App.js
@@ -96,9 +96,37 @@ class App extends Component{
                data={this.state.contents}></TOC>
 
                <Control onChangeMode = {function(_mode){
-                this.setState({
-                    mode:_mode
-                });
+
+                if(_mode === 'delete'){
+                    
+                    if(_mode === 'delete'){
+                        if(window.confirm('really?')){
+                            var _contents = Array.from(this.state.contents);
+                            var i = 0;
+                            while(i < _contents.length){
+                                if(_contents[i].id === this.state.selected_content_id){
+                                    _contents.splice(i,1);
+                                    break;
+                                }
+                                i = i+1;
+                            }
+                            
+                            this.setState({
+                                mode:'welcome',
+                                contents:_contents
+                            })
+                        }
+                    }
+
+                }else{
+
+                    this.setState({
+                        mode:_mode
+                    });
+
+                }
+
+
                }.bind(this)}></Control>
                
                {this.getContent()}
