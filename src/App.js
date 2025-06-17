0db834ad35165167538148b05e14156dbf3b6974
bitna <hbn1218@naver.com>
2025-06-18 02:19:37 +0900

    미니프로젝트 카운터앱

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-안녕
+        <div>{count}</div>
+        <div className="button-group">
+          <button onClick={()=> setCount(count+1)}>+</button>
+          <button onClick={()=>{ 
+            if(count-1 < 0){
+              alert('마이너스 !!!');
+              return
+            }
+            setCount(count-1)     
+            }}>-</button>
+          <button onClick={()=> setCount(0) }>Reset</button>
+        </div>
+
     </div>
   );
 }
