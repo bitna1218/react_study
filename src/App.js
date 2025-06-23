3dc3e21255bfb387f80eeace68998cd6a2d5e661
bitna <hbn1218@naver.com>
2025-06-23 23:58:03 +0900

    미니프로젝트 할일목록만들기_추가할때엔터기능 몇 input창리셋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState,useRef } from 'react';
 
 function App() {
   const [todoList,setTodoList] = useState([]);
   const [value,setValue] = useState("");
+  const indexVal = useRef(0);
+
+  function addEvent(){
+    
+    setTodoList([...todoList, { id: indexVal.current, text:value, done: false }])
+    indexVal.current+=1
+    setValue("");
+
+  }
+
+  function inputEvent(e){
+    setValue(e.target.value);
+  }
+
+  function enterEvent(e){
+    if(e.key === 'Enter'){
+      addEvent();
+    }
+  }
+
+  function checkboxEvent(){
+    alert('sss')
+  }
+
   return (
     <div className="App">
-      <input type='text' onChange={function(e){
-        
-        setValue(e.target.value);
-
-      }}/>
-      <button onClick={function(){
-        
-        setTodoList([...todoList, value])
-
-      }}>추가</button>
+      <input type='text' value={value} onChange={inputEvent} onKeyDown={enterEvent}/>
+      <button onClick={addEvent}>추가</button>
       
       <div>
-        {todoList.map((item, index) => (
-          <div key={index}>{item}</div>
+        {todoList.map((item) => (
+          <div key={item.id} id={item.id}><input type='checkbox' onChange={checkboxEvent}/>{item.text}</div>
         ))}
       </div>
       
