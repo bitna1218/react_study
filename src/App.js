cf8c0e6e6cda2e48efe6e239fda7071f2b1bf39b
bitna <hbn1218@naver.com>
2025-07-03 15:53:18 +0900

    미니프로젝트 실시간시계_기능구현

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import './App.css';
-import Card from "./Card.js"
+import { useState, useEffect } from 'react';
+
+function Clock(){
+
+  const [time, setTime] = useState(0);
+
+  useEffect(()=>{
+    const intervalId = setInterval(()=>{
+      setTime(new Date())
+    },1000);
+
+    return () => clearInterval(intervalId);
+
+  },[]);
+
+  return(
+    <div>
+      <h2>현재 시간</h2>
+      <p>{time.toLocaleString()}</p>
+    </div>
+  )
+}
+
 
 function App() {
 
   return (
-    <div className="App">
-
-      <div className="container rounded-md border-2 h-screen mx-auto flex flex-wrap justify-evenly gap-x-8 items-center">
-        <Card name="오로라핑" role="Front-End" img="https://i.namu.wiki/i/tOsJUxCFXQxjXRuPdKyQYkVSvBiWfLR1OHAkVx7HF0FvzebhOrj1ls7LQkmzBvtn-zXQADX4u9jB8Fq5reGNpQ.webp"/>
-        <Card name="하츄핑" role="Back-End" img="https://i.namu.wiki/i/Vj5qbEFSnNirgU_WzuKbQmLd20hbM6QyNGHb8f87wB4iUuMA-OliDHoQMBnxu7jSowmBl5R-wBKXIb5Voe1bxw.webp"/>
-        <Card name="시러핑" role="Engineer" img="https://i.namu.wiki/i/La-Y_f25NlQ9Jrw2e7Hol2aDvl4GP7e4RLhd-E5Bi0qNGXH1vaQG7KigZU8rad9kopFoAXWyZXRcksYEd49E87rblVvF6lVzEKJNtzqCmo8Keu1u1HkHEAzeunjY6CPUEe3GM1nTJQZO41LGEtjLJg.webp"/>
-                <Card name="오로라핑" role="Front-End" img="https://i.namu.wiki/i/tOsJUxCFXQxjXRuPdKyQYkVSvBiWfLR1OHAkVx7HF0FvzebhOrj1ls7LQkmzBvtn-zXQADX4u9jB8Fq5reGNpQ.webp"/>
-        <Card name="하츄핑" role="Back-End" img="https://i.namu.wiki/i/Vj5qbEFSnNirgU_WzuKbQmLd20hbM6QyNGHb8f87wB4iUuMA-OliDHoQMBnxu7jSowmBl5R-wBKXIb5Voe1bxw.webp"/>
-        <Card name="시러핑" role="Engineer" img="https://i.namu.wiki/i/La-Y_f25NlQ9Jrw2e7Hol2aDvl4GP7e4RLhd-E5Bi0qNGXH1vaQG7KigZU8rad9kopFoAXWyZXRcksYEd49E87rblVvF6lVzEKJNtzqCmo8Keu1u1HkHEAzeunjY6CPUEe3GM1nTJQZO41LGEtjLJg.webp"/>
-                <Card name="오로라핑" role="Front-End" img="https://i.namu.wiki/i/tOsJUxCFXQxjXRuPdKyQYkVSvBiWfLR1OHAkVx7HF0FvzebhOrj1ls7LQkmzBvtn-zXQADX4u9jB8Fq5reGNpQ.webp"/>
-        <Card name="하츄핑" role="Back-End" img="https://i.namu.wiki/i/Vj5qbEFSnNirgU_WzuKbQmLd20hbM6QyNGHb8f87wB4iUuMA-OliDHoQMBnxu7jSowmBl5R-wBKXIb5Voe1bxw.webp"/>
-        <Card name="시러핑" role="Engineer" img="https://i.namu.wiki/i/La-Y_f25NlQ9Jrw2e7Hol2aDvl4GP7e4RLhd-E5Bi0qNGXH1vaQG7KigZU8rad9kopFoAXWyZXRcksYEd49E87rblVvF6lVzEKJNtzqCmo8Keu1u1HkHEAzeunjY6CPUEe3GM1nTJQZO41LGEtjLJg.webp"/>
-                <Card name="오로라핑" role="Front-End" img="https://i.namu.wiki/i/tOsJUxCFXQxjXRuPdKyQYkVSvBiWfLR1OHAkVx7HF0FvzebhOrj1ls7LQkmzBvtn-zXQADX4u9jB8Fq5reGNpQ.webp"/>
-        <Card name="하츄핑" role="Back-End" img="https://i.namu.wiki/i/Vj5qbEFSnNirgU_WzuKbQmLd20hbM6QyNGHb8f87wB4iUuMA-OliDHoQMBnxu7jSowmBl5R-wBKXIb5Voe1bxw.webp"/>
-        <Card name="시러핑" role="Engineer" img="https://i.namu.wiki/i/La-Y_f25NlQ9Jrw2e7Hol2aDvl4GP7e4RLhd-E5Bi0qNGXH1vaQG7KigZU8rad9kopFoAXWyZXRcksYEd49E87rblVvF6lVzEKJNtzqCmo8Keu1u1HkHEAzeunjY6CPUEe3GM1nTJQZO41LGEtjLJg.webp"/>
-                <Card name="오로라핑" role="Front-End" img="https://i.namu.wiki/i/tOsJUxCFXQxjXRuPdKyQYkVSvBiWfLR1OHAkVx7HF0FvzebhOrj1ls7LQkmzBvtn-zXQADX4u9jB8Fq5reGNpQ.webp"/>
-        <Card name="하츄핑" role="Back-End" img="https://i.namu.wiki/i/Vj5qbEFSnNirgU_WzuKbQmLd20hbM6QyNGHb8f87wB4iUuMA-OliDHoQMBnxu7jSowmBl5R-wBKXIb5Voe1bxw.webp"/>
-        <Card name="시러핑" role="Engineer" img="https://i.namu.wiki/i/La-Y_f25NlQ9Jrw2e7Hol2aDvl4GP7e4RLhd-E5Bi0qNGXH1vaQG7KigZU8rad9kopFoAXWyZXRcksYEd49E87rblVvF6lVzEKJNtzqCmo8Keu1u1HkHEAzeunjY6CPUEe3GM1nTJQZO41LGEtjLJg.webp"/>
+      <div className="App">
+        <Clock/>
       </div>
+    );
 
-    </div>
-  );
 }
 
 export default App;
